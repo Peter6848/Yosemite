71be951d9e8e9035f56b59b8fbaba35723554bf1
Peter Thompson <peterthompson@roamflyfishing.com>
2017-10-12 12:37:12 -0700

    Move trip form partial into trips view, make js for toggling form functional.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,13 +50,8 @@ var showTripInfoListener = function(){
 var bookTripListener = function(){
   $('.book-button').on('click', function(e){
     e.preventDefault();
+    var bookingForm = $(this).closest('.trips').find('.trip-form');
     $(this).css('background-color', '#272728');
-    var clickedButton = $('.container').closest('.book-button');
-    $(this).find('.trips').append('.book-trip-form');
-    $('.trip-form').toggle();
-
-    // var url = $(clickedButton).attr('href');
-    // var method = $(clickedButton).attr('method');
-    // var data = $()
+    $(bookingForm).toggle();
   })
 }
