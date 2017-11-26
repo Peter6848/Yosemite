51b60d516cd26215c6fb820fa55f27557157d4df
Peter Thompson <peterthompson@roamflyfishing.com>
2017-11-26 09:32:39 -0800

    Add full name method for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :trips
   has_many :user_trips
   has_many :trips, through: :user_trips
+
+  validates :first_name, :last_name, :email, :password, :phone_number, presence: true
+  validates :email, :phone_number, uniqueness: true
+  has_secure_password
+
+  def full_name()
+    self.first_name + ' ' + self.last_name
+  end
+
+
+
 end
